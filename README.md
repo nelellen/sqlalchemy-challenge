cc3c03a1daeba117297b245087586931a4a9e20c
nelellen <nelsonellen80@yahoo.com>
2024-09-15 11:56:41 -0400

    Getting Errors for Attribute 'measurement' unable to debug

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sqlalchemy-challenge
+
+Analyzing weather data in Honolulu for my Vacation!
+
+Overview of the Analysis
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,97 @@
 # Import the dependencies.
-
-
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify, request
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
-
+Base.prepare(autoload_with=engine)
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+try:
+    Measurement = Base.classes.measurement
+except AttributeError as e:
+    print(f"Error accessing 'measurement': {e}")
+    Measurement = None
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   past_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= past_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    past_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.tobs).\
+    filter(Measurement.station == 'USC00519281').\
+    filter(Measurement.date >= past_year).all()
+    temps = list(np.ravel(results))
+    
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<end>")
+
+def stats(start=None, end=None):
+        sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
+    
+if not end: 
+       results = session.query(sel).\
+        filter(Measurement.date <= start).all()
+       temps = list(np.ravel(results))
+return jsonify(temps)
+
+results = session.query(sel).\
+           filter(Measurement.date >= start).\
+         filter(Measurement.date <= end).all()
+temps = list(np.ravel(results))
+return jsonify(temps=temps)
+
+if __name__ =='__main__':
+    app.run(debug=true)
